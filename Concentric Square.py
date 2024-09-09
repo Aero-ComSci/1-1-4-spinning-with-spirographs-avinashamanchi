13940c71f77b0f9c2eef840a37f5e56350dd0f3b
Avinash Amanchi <148393523+avinashamanchi@users.noreply.github.com>
2024-09-09 13:51:58 -0700

    Update Concentric Square.py

--- Concentric Square.py
+++ Concentric Square.py
@@ -10,43 +10,46 @@ def draw_square(turtle_obj, size):
 
 def draw_concentric_squares_with_colors(num_squares, colors):
     screen = turtle.Screen()
-    screen.setup(width=800, height=800)  
-    screen.bgcolor("white") 
+    screen.setup(width=800, height=800)
+    screen.bgcolor("white")
     
     turtle_obj = turtle.Turtle()
-    turtle_obj.speed(0) 
+    turtle_obj.speed(0)
     turtle_obj.hideturtle()
     
-    turtle_obj.width(5) 
+    turtle_obj.width(5)
     
-    size = 20 
+    size_increment = 800 / num_squares
+    initial_size = size_increment
+    
     num_colors = len(colors)
     
     for i in range(num_squares):
         turtle_obj.penup()
-        turtle_obj.goto(-size / 2, size / 2)
+        turtle_obj.goto(-initial_size / 2, initial_size / 2)
         turtle_obj.pendown()
         
         color = rgb_to_hex(colors[i % num_colors])
         turtle_obj.color(color)
         
-        draw_square(turtle_obj, size)
+        draw_square(turtle_obj, initial_size)
         
-        size += 20  
+        initial_size += size_increment
 
-    screen.mainloop()  
+    screen.mainloop()
 
 colors = [
-    (255, 0, 0),   
-    (0, 255, 0),    
-    (0, 0, 255),    
-    (0, 255, 255),  
-    (255, 0, 255),  
-    (255, 255, 0),  
-    (255, 165, 0),  
-    (128, 0, 128),  
-    (0, 128, 128),  
-    (128, 128, 128) 
+    (255, 0, 0),
+    (0, 255, 0),
+    (0, 0, 255),
+    (0, 255, 255),
+    (255, 0, 255),
+    (255, 255, 0),
+    (255, 165, 0),
+    (128, 0, 128),
+    (0, 128, 128),
+    (128, 128, 128)
 ]
 
-draw_concentric_squares_with_colors(10, colors)
+draw_concentric_squares_with_colors(20, colors)
+
