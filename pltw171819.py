7f925257d0c5bb2c5875315643f18386c045565f
Avinash Amanchi <148393523+avinashamanchi@users.noreply.github.com>
2024-09-11 13:54:35 -0700

    Update pltw171819.py

--- pltw171819.py
+++ pltw171819.py
@@ -6,7 +6,6 @@ painter.goto(-200, 0)
 painter.pendown()
 
 while True:
-    # Original pattern
     x = -200
     y = 0
     move_x = 1
@@ -25,43 +24,27 @@ while True:
             painter.goto(x, y)
         move_y = 1
 
-    # Reset position for the opposite pattern
     painter.penup()
     painter.goto(-200, 0)
     painter.pendown()
 
-    # Opposite pattern
     x = -200
     y = 0
     move_x = 1
-    move_y = 1
+    move_y = -1
 
     while (x < 200):
-        while (y > -100):  # Move downwards for the opposite pattern
+        while (y > -100):
             x = x + move_x
             y = y + move_y
             painter.goto(x, y)
         move_y = 1
 
-        while (y < 0):  # Move upwards for the opposite pattern
+        while (y < 0):
             x = x + move_x
             y = y + move_y
             painter.goto(x, y)
         move_y = -1
-        
-        while (x < 200):
-          while (y > -100):  # Move downwards for the opposite pattern
-            x = x + move_x
-            y = y + move_y
-            painter.goto(x, y)
-        move_y = 1
-        
-        while (y < 0):  # Move upwards for the opposite pattern
-            x = x + move_x
-            y = y + move_y
-            painter.goto(x, y)
-        move_y = -1
-
 
 wn = trtl.Screen()
 wn.mainloop()
